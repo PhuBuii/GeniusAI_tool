35d0000e7cdc7ef46188640a9deefa934774bd15
PhuBui <buiquangphu.work@gmail.com>
2024-10-25 00:08:20 +0700

    add generate image & generate music

--- components/empty.tsx
+++ components/empty.tsx
@@ -10,7 +10,10 @@ const Empty = ({ label, type }: EmptyProps) => {
     <div className="h-full p-20 flex flex-col items-center justify-center">
       <div className="relative h-72 w-72">
         {type === "code" && <Image alt="Empty" fill src="/empty_code.png" />}
+        {type === "image" && <Image alt="Empty" fill src="/empty_image.png" />}
         {type === "conversation" && <Image alt="Empty" fill src="/empty.png" />}
+        {type === "music" && <Image alt="Empty" fill src="/empty_music.png" />}
+        {type === "video" && <Image alt="Empty" fill src="/empty_video.jpg" />}
       </div>
       <p className="text-muted-foreground text-md text-center">{label}</p>
     </div>
